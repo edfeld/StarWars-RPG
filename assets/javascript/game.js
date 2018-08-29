3995d6fc5f57e0e9af665c42c8c834dc86d1b252
edfeld <edfeld1@gmail.com>
2018-08-29 10:29:32 -0700

    just getting started

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+// Star Wars JavaScript
+
+let starWarsGame = {
+    wordsToPick: {
+        obiwan: {
+            name: "Obi Wan",
+            picture: "obi.jpg",
+            healthPoints: 10,
+            hitPoints: 5,
+        },
+        darthmaul: {
+            name: "Darth Maul",
+            picture: "darthmaul.jpg",
+            healthPoints: 10,
+            hitPoints: 5,
+        },
+        lukeskywalker: {
+            name: "Luke Skywalker",
+            picture: "luke.jpg",
+            healthPoints: 10,
+            hitPoints: 5,
+        },
+        darthsideous: {
+            name: "Darth Sidious",
+            picture: "darthsidious.jpg",
+            healthPoints: 10,
+            hitPoints: 5,
+        },
+
+      },
+      
+      randArr: []
+}
